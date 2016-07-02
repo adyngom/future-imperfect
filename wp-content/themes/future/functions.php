55d5e48d3311a25c98c4b7b1390f8dc4981e5482
Ady Ngom <forthehackofit>
2016-07-02 17:22:47 -0400

    all styles ar wp now

--- wp-content/themes/future/functions.php
+++ wp-content/themes/future/functions.php
@@ -2,7 +2,8 @@
 	/**
 	* Define constants
 	*/
-	define('FUTURE_STYLE_URI', get_stylesheet_directory_uri());
+	define('FUTURE_MAIN_STYLE', get_stylesheet_uri());
+	define('FUTURE_TEMPLATE_PATH', get_template_directory_uri());
 	/**
 	 * Enqueues scripts and styles.
 	 *
@@ -10,20 +11,26 @@
 	 */
 	function future_scripts() {
 
-		global $wp_styles;
-		// add font awesome first
+		// add html5 shiv if IE8
+		wp_enqueue_script('html5-shiv', FUTURE_TEMPLATE_PATH . '/assets/js/ie/html5shiv.js');
+		wp_script_add_data('html5-shiv', 'conditional', 'lte IE 8');
+
+		// add font awesome
 		wp_enqueue_script('font-awesome', 'https://use.fontawesome.com/9d56bc066f.js');
 
 		// add google fonts
 		wp_enqueue_style('font-raleway', 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700|Raleway:400,800,900');
 
+		// add main theme stylesheet
+		wp_enqueue_style('future-style', FUTURE_MAIN_STYLE);
+
 		// add conditional IE 9
-		wp_enqueue_style('future-ie-9', FUTURE_STYLE_URI);
-		$wp_styles->add_data('future-ie-9', 'conditional', 'lt IE 9');
+		wp_enqueue_style('future-ie-9', FUTURE_TEMPLATE_PATH . '/assets/css/ie9.css');
+		wp_style_add_data('future-ie-9', 'conditional', 'lt IE 9');
 
 		// add conditional IE 8
-		wp_enqueue_style('future-ie-8', FUTURE_STYLE_URI);
-		$wp_styles->add_data('future-ie-8', 'conditional', 'lt IE 8');
+		wp_enqueue_style('future-ie-8', FUTURE_TEMPLATE_PATH . '/assets/css/ie8.css');
+		wp_style_add_data('future-ie-8', 'conditional', 'lt IE 8');
 	}
 	add_action('wp_enqueue_scripts', 'future_scripts');
 ?>
--- wp-content/themes/future/header.php
+++ wp-content/themes/future/header.php
@@ -9,8 +9,6 @@ Free for personal and commercial use under the CCA 3.0 license (html5up.net/lice
 <title>Future Imperfect by HTML5 UP</title>
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
-<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <?php wp_head(); ?>
 </head>
 <body>
