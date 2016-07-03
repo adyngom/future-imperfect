86cbb075a91601396f9be66cb99062fa73d5fb0c
Ady Ngom <forthehackofit>
2016-07-02 20:52:24 -0400

    add images temporarily

--- wp-content/themes/future/index.php
+++ wp-content/themes/future/index.php
@@ -15,63 +15,11 @@
 						</div>
 						<div class="meta">
 							<time class="published" datetime="2015-11-01">November 1, 2015</time>
-							<a href="#" class="author"><span class="name">Jane Doe</span><img src="images/avatar.jpg" alt="" /></a>
+							<a href="#" class="author"><span class="name">Jane Doe</span><img src="<?php echo get_template_directory_uri();?>/images/avatar.jpg" alt="" /></a>
 						</div>
 					</header>
-					<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+					<a href="#" class="image featured"><img src="<?php echo get_template_directory_uri();?>/images/pic01.jpg" alt="" /></a>
 					<p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
-					<footer>
-						<ul class="actions">
-							<li><a href="#" class="button big">Continue Reading</a></li>
-						</ul>
-						<ul class="stats">
-							<li><a href="#">General</a></li>
-							<li><a href="#" class="icon fa-heart">28</a></li>
-							<li><a href="#" class="icon fa-comment">128</a></li>
-						</ul>
-					</footer>
-				</article>
-
-			<!-- Post -->
-				<article class="post">
-					<header>
-						<div class="title">
-							<h2><a href="#">Ultricies sed magna euismod enim vitae gravida</a></h2>
-							<p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
-						</div>
-						<div class="meta">
-							<time class="published" datetime="2015-10-25">October 25, 2015</time>
-							<a href="#" class="author"><span class="name">Jane Doe</span><img src="images/avatar.jpg" alt="" /></a>
-						</div>
-					</header>
-					<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
-					<p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper.</p>
-					<footer>
-						<ul class="actions">
-							<li><a href="#" class="button big">Continue Reading</a></li>
-						</ul>
-						<ul class="stats">
-							<li><a href="#">General</a></li>
-							<li><a href="#" class="icon fa-heart">28</a></li>
-							<li><a href="#" class="icon fa-comment">128</a></li>
-						</ul>
-					</footer>
-				</article>
-
-			<!-- Post -->
-				<article class="post">
-					<header>
-						<div class="title">
-							<h2><a href="#">Euismod et accumsan</a></h2>
-							<p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
-						</div>
-						<div class="meta">
-							<time class="published" datetime="2015-10-22">October 22, 2015</time>
-							<a href="#" class="author"><span class="name">Jane Doe</span><img src="images/avatar.jpg" alt="" /></a>
-						</div>
-					</header>
-					<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
-					<p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla. Cras vehicula tellus eu ligula viverra, ac fringilla turpis suscipit. Quisque vestibulum rhoncus ligula.</p>
 					<footer>
 						<ul class="actions">
 							<li><a href="#" class="button big">Continue Reading</a></li>
--- wp-content/themes/future/sidebar.php
+++ wp-content/themes/future/sidebar.php
@@ -2,7 +2,7 @@
 
 	<!-- Intro -->
 		<section id="intro">
-			<a href="#" class="logo"><img src="images/logo.jpg" alt="" /></a>
+			<a href="#" class="logo"><img src="<?php echo get_template_directory_uri();?>/images/logo.jpg" alt="" /></a>
 			<header>
 				<h2>Future Imperfect</h2>
 				<p>Another fine responsive site template by <a href="http://html5up.net">HTML5 UP</a></p>
@@ -18,9 +18,9 @@
 						<header>
 							<h3><a href="#">Vitae sed condimentum</a></h3>
 							<time class="published" datetime="2015-10-20">October 20, 2015</time>
-							<a href="#" class="author"><img src="images/avatar.jpg" alt="" /></a>
+							<a href="#" class="author"><img src="<?php echo get_template_directory_uri();?>/images/avatar.jpg" alt="" /></a>
 						</header>
-						<a href="#" class="image"><img src="images/pic04.jpg" alt="" /></a>
+						<a href="#" class="image"><img src="<?php echo get_template_directory_uri();?>/images/pic04.jpg" alt="" /></a>
 					</article>
 
 				<!-- Mini Post -->
@@ -28,9 +28,9 @@
 						<header>
 							<h3><a href="#">Rutrum neque accumsan</a></h3>
 							<time class="published" datetime="2015-10-19">October 19, 2015</time>
-							<a href="#" class="author"><img src="images/avatar.jpg" alt="" /></a>
+							<a href="#" class="author"><img src="<?php echo get_template_directory_uri();?>/images/avatar.jpg" alt="" /></a>
 						</header>
-						<a href="#" class="image"><img src="images/pic05.jpg" alt="" /></a>
+						<a href="#" class="image"><img src="<?php echo get_template_directory_uri();?>/images/pic05.jpg" alt="" /></a>
 					</article>
 
 				<!-- Mini Post -->
@@ -38,9 +38,9 @@
 						<header>
 							<h3><a href="#">Odio congue mattis</a></h3>
 							<time class="published" datetime="2015-10-18">October 18, 2015</time>
-							<a href="#" class="author"><img src="images/avatar.jpg" alt="" /></a>
+							<a href="#" class="author"><img src="<?php echo get_template_directory_uri();?>/images/avatar.jpg" alt="" /></a>
 						</header>
-						<a href="#" class="image"><img src="images/pic06.jpg" alt="" /></a>
+						<a href="#" class="image"><img src="<?php echo get_template_directory_uri();?>/images/pic06.jpg" alt="" /></a>
 					</article>
 
 				<!-- Mini Post -->
@@ -48,9 +48,9 @@
 						<header>
 							<h3><a href="#">Enim nisl veroeros</a></h3>
 							<time class="published" datetime="2015-10-17">October 17, 2015</time>
-							<a href="#" class="author"><img src="images/avatar.jpg" alt="" /></a>
+							<a href="#" class="author"><img src="<?php echo get_template_directory_uri();?>/images/avatar.jpg" alt="" /></a>
 						</header>
-						<a href="#" class="image"><img src="images/pic07.jpg" alt="" /></a>
+						<a href="#" class="image"><img src="<?php echo get_template_directory_uri();?>/images/pic07.jpg" alt="" /></a>
 					</article>
 
 			</div>
@@ -65,7 +65,7 @@
 							<h3><a href="#">Lorem ipsum fermentum ut nisl vitae</a></h3>
 							<time class="published" datetime="2015-10-20">October 20, 2015</time>
 						</header>
-						<a href="#" class="image"><img src="images/pic08.jpg" alt="" /></a>
+						<a href="#" class="image"><img src="<?php echo get_template_directory_uri();?>/images/pic08.jpg" alt="" /></a>
 					</article>
 				</li>
 				<li>
@@ -74,7 +74,7 @@
 							<h3><a href="#">Convallis maximus nisl mattis nunc id lorem</a></h3>
 							<time class="published" datetime="2015-10-15">October 15, 2015</time>
 						</header>
-						<a href="#" class="image"><img src="images/pic09.jpg" alt="" /></a>
+						<a href="#" class="image"><img src="<?php echo get_template_directory_uri();?>/images/pic09.jpg" alt="" /></a>
 					</article>
 				</li>
 				<li>
@@ -83,7 +83,7 @@
 							<h3><a href="#">Euismod amet placerat vivamus porttitor</a></h3>
 							<time class="published" datetime="2015-10-10">October 10, 2015</time>
 						</header>
-						<a href="#" class="image"><img src="images/pic10.jpg" alt="" /></a>
+						<a href="#" class="image"><img src="<?php echo get_template_directory_uri();?>/images/pic10.jpg" alt="" /></a>
 					</article>
 				</li>
 				<li>
@@ -92,7 +92,7 @@
 							<h3><a href="#">Magna enim accumsan tortor cursus ultricies</a></h3>
 							<time class="published" datetime="2015-10-08">October 8, 2015</time>
 						</header>
-						<a href="#" class="image"><img src="images/pic11.jpg" alt="" /></a>
+						<a href="#" class="image"><img src="<?php echo get_template_directory_uri();?>/images/pic11.jpg" alt="" /></a>
 					</article>
 				</li>
 				<li>
@@ -101,7 +101,7 @@
 							<h3><a href="#">Congue ullam corper lorem ipsum dolor</a></h3>
 							<time class="published" datetime="2015-10-06">October 7, 2015</time>
 						</header>
-						<a href="#" class="image"><img src="images/pic12.jpg" alt="" /></a>
+						<a href="#" class="image"><img src="<?php echo get_template_directory_uri();?>/images/pic12.jpg" alt="" /></a>
 					</article>
 				</li>
 			</ul>
